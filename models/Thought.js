6087bf361755b84178333d7ac9cade4cdc190350
John Tannous <john.tannous97@gmail.com>
2022-02-11 20:45:20 -0800

    setting up atlas

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+// Schema to create Student model
+const thoughtSchema = new Schema(
+  {
+      thoughtText:{
+        type: String,
+        required: true,
+        min_length: 1,
+        max_length: 280,
+      },
+      createdAt:{
+        type: Date,
+        default: Date.now(),
+        get:(date) => new Date(date).toLocaleDateString(),
+      },
+      username:
+          {
+            type: String,
+            required: true,
+          },
+      reactions:[
+       reactionSchema
+      ]
+    
+  },
+  {
+    toJSON: {
+      virtuals:true,
+      getters: true,
+    },
+    
+    id:false,
+
+  }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
